57fa9a07713479ce7bb3fca57e4f825072f452d6
Ruslan379 <3325528@ukr.net>
2026-06-17 15:21:35 +0300

    New step 229

--- src/components/App/AppComplexForms.jsx
+++ src/components/App/AppComplexForms.jsx
@@ -1,12 +1,14 @@
 // console.log(
-//   "%c 4.4.3.Складні форми + \n 4.4.4.Генерація Id елементів форми + \n 4.4.5.Радіокнопки ",
+//   "%c 4.4.3.Складні форми + \n 4.4.4.Генерація Id елементів форми + \n 4.4.5.Радіокнопки + \n 4.4.6.Чекбокси" ,
 //   "color: white; background-color: #D33F49",
 // );
 
 import React, { Component } from "react";
 import { ComplexForms } from '@/components/ComplexForms/ComplexForms.jsx';
 import { ComplexFormsGenerationID } from '@/components/ComplexFormsGenerationID/ComplexFormsGenerationID.jsx';
-import { ComplexFormsGenerationIDRadioButton} from '@/components/ComplexFormsGenerationIDRadioButton/ComplexFormsGenerationIDRadioButton.jsx';
+import { ComplexFormsGenerationIDRadioButton } from '@/components/ComplexFormsGenerationIDRadioButton/ComplexFormsGenerationIDRadioButton.jsx';
+import { ComplexFormsGenerationIDRadioButtonCheckboxes } from '@/components/ComplexFormsGenerationIDRadioButtonCheckboxes/ComplexFormsGenerationIDRadioButtonCheckboxes.jsx';
+
 // import css from "./AppComplexForms.module.css";
 
 export class AppComplexForms extends Component {
@@ -49,9 +51,9 @@ export class AppComplexForms extends Component {
         {/* <ComplexFormsGenerationID onSubmit={this.submitForm} /> */}
         {/* <ComplexFormsGenerationID onSubmit={this.submitForm} /> */}
         {/*//! + 4.4.5.Радіокнопки */}
-        <ComplexFormsGenerationIDRadioButton
-          onSubmit={this.submitForm}
-        />
+        {/* <ComplexFormsGenerationIDRadioButton onSubmit={this.submitForm} /> */}
+        {/*//! + 4.4.6.Чекбокси */}
+        <ComplexFormsGenerationIDRadioButtonCheckboxes onSubmit={this.submitForm} />
       </>
     );
   }
---
+++ src/components/ComplexFormsGenerationIDRadioButtonCheckboxes/ComplexFormsGenerationIDRadioButtonCheckboxes.jsx
@@ -0,0 +1,173 @@
+import React, { Component } from "react";
+import { nanoid } from 'nanoid' //! для генерації Id елементів форми
+import css from "./ComplexFormsGenerationIDRadioButtonCheckboxes.module.css";
+
+const INITIAL_STATE = {
+    inputLogin: "",
+    inputPassword: "",
+    experience: "junior",
+    licence: false
+};
+
+
+export class ComplexFormsGenerationIDRadioButtonCheckboxes extends Component {
+    state = { ...INITIAL_STATE };
+
+    //! Для генерації Id елементів форми:
+    //! Для кожного інпуту робимо окрему властивість класу:
+    loginInputId = nanoid();
+    passwordInputId = nanoid();
+
+    //! Скидання state в початкове значення INITIAL_STATE
+    reset = () => {
+        this.setState({ ...INITIAL_STATE });
+    };
+
+    // todo: NEW
+    handleSubmit = event => {
+        event.preventDefault();
+        const { inputLogin, inputPassword } = this.state;
+        console.log(`🈂️Login: ${inputLogin}, 🈳Password: ${inputPassword}`);
+        this.props.onSubmit({ ...this.state }); //! підняття стану + передача state в AppComplexForms.jsx
+        this.reset();  //! очищуємо поля всіх інпутів
+    };
+
+    handleChange = event => {
+        // console.log("event.currentTarget:", event.currentTarget);
+        // console.log("event.currentTarget.value:", event.currentTarget.name);
+        // console.log("event.currentTarget.value:", event.currentTarget.value);
+
+        //! Деструктуризуємо:
+        const { name, value } = event.currentTarget;
+        //! Зберігаємо значення інпутів в state, використовуючи властивості об'єкта, що обчислюються
+        this.setState({
+            [name]: value,
+        });
+    };
+
+    handleChangeCheckbox = event => {
+        console.log("event.currentTarget.checked:", event.currentTarget.checked);
+
+
+        // //! Деструктуризуємо:
+        // const { name, value } = event.currentTarget;
+        // //! Зберігаємо значення інпутів в state, використовуючи властивості об'єкта, що обчислюються
+        // this.setState({
+        //     [name]: value,
+        // });
+
+        this.setState({ licence: event.currentTarget.checked});
+    };
+
+
+    render() {
+        const {
+            inputLogin,
+            inputPassword,
+            experience,
+            licence
+        } = this.state;
+
+        console.log("----------------------------------------------");
+        console.log("🛅Значення inputLogin:", inputLogin);
+        console.log("🛅Значення inputPassword:", inputPassword);
+        console.log("🛅Значення experience:", experience);
+        console.log("🛅Значення licence:", licence);
+        console.log("______________________________________________");
+
+        return (
+            <form
+                className={css.loginForm}
+                onSubmit={this.handleSubmit}
+            >
+                {/*//! 4.4.3.Складні форми + 4.4.4.Генерація Id елементів форми */}
+                <label
+                    className={css.loginFormLabel}
+                    // htmlFor="username"
+                    htmlFor={this.loginInputId} //? для генерації Id елементів форми
+                >
+                    Логін:
+                </label>
+                <input
+                    className={css.loginFormInput}
+                    type="text"
+                    name="inputLogin"
+                    // id="username"
+                    id={this.loginInputId} //? для генерації Id елементів форми
+                    value={inputLogin}
+                    onChange={this.handleChange}
+                />
+
+                <label
+                    className={css.loginFormLabel}
+                    // htmlFor="pwd"
+                    htmlFor={this.passwordInputId} //* для генерації Id елементів форми
+                >
+                    Пароль:
+                </label>
+                <input
+                    className={css.loginFormInput}
+                    type="password"
+                    name="inputPassword"
+                    // id="pwd"
+                    id={this.passwordInputId} //* для генерації Id елементів форми
+                    value={inputPassword}
+                    onChange={this.handleChange}
+                />
+
+                {/*//! + 4.4.5.Радіокнопки */}
+                <label>
+                    Junior
+                    <input
+                        type="radio"
+                        name="experience"
+                        value="junior"
+                        checked={this.state.experience === "junior"}
+                        onChange={this.handleChange}
+                    />
+                </label>
+
+                <label>
+                    Middle
+                    <input
+                        type="radio"
+                        name="experience"
+                        value="middle"
+                        checked={this.state.experience === "middle"}
+                        onChange={this.handleChange}
+                    />
+                </label>
+
+                <label>
+                    Senior
+                    <input
+                        type="radio"
+                        name="experience"
+                        value="senior"
+                        checked={this.state.experience === "senior"}
+                        onChange={this.handleChange}
+                    />
+                </label>
+
+                {/*//! + 4.4.6.Чекбокс */}
+                <label>
+                    Згоден з умовами
+                    <input
+                        type="checkbox"
+                        name="licence"
+                        checked={licence}
+                        onChange={this.handleChangeCheckbox}
+                    />
+                </label>
+
+                <button
+                    className={css.loginButton}
+                    type="submit"
+                    disabled={!licence} //! блокування кнопки чекбоксом
+                >
+                    Submit
+                </button>
+            </form>
+        );
+    }
+};
